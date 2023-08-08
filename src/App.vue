ecb843930b5562eefd41145d389b0f6c6b3e8e5e
mansoor250 <mohammedmansoor250@gmail.com>
2023-08-09 00:33:22 +0530

    Modal proj

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
- <h1>{{ title }}</h1>
+ <h1>{{ title }}</h1><br>
+ <input type="text" ref="name">
+ <button @click="handleClick"> Click Me</button>
+ <p>Welcome....</p>
+ <div v-if="showModal">
+  <Modal :header='header' :text='text' theme ='sale' @click="toggleModal"/> 
+ </div>
+ <button @click="toggleModal">Open Modal</button>
 </template>
 
 <script> 
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  components:{ Modal },
  data(){
   return{
-     title: 'My First Vue App :)'
+     title: 'My First Vue App :-)',
+     header: "Sign up for Giveaway!",
+     text:'Sign in to your account!' ,
+     showModal:false
+
+  }
+ },
+ methods: {
+  handleClick(){
+    console.log (this.$refs.name)
+    this.$refs.name.classList.add('active')
+    this.$refs.name.focus()
+  },
+  toggleModal() {
+    this.showModal=!this.showModal
   }
  }
+
 }
 </script>
 
@@ -26,5 +51,8 @@ h1{
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
+}
+button{
+  background-color: rgba(15, 138, 44, 0.193);
 }
 </style>
