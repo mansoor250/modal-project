73b1989b931d9d9c2688db60350cfaa6ce77b0ec
mansoor250 <mohammedmansoor250@gmail.com>
2023-08-09 12:07:14 +0530

    slots

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 80px;
 }
 h1{
   border-bottom: 1px solid #ddd;
