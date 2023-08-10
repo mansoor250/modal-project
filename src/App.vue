7f7ee52b22253c3d7fc6b82638b08bf29d35072f
mansoor250 <mohammedmansoor250@gmail.com>
2023-08-10 11:23:13 +0530

    Slots and Modal component usding teleport

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,74 @@
 <template>
- <h1>{{ title }}</h1><br>
- <input type="text" ref="name">
- <button @click="handleClick"> Click Me</button>
- <p>Welcome....</p>
- <div v-if="showModal">
-  <Modal :header='header' :text='text' theme ='sale' @click="toggleModal"/> 
- </div>
- <button @click.alt="toggleModal">Open Modal</button>
+  <h1>{{ title }}</h1>
+  <br />
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click Me</button>
+  <p>Welcome....</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @click="toggleModal"> 
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>New Modal Project Giveaway!..</h1>
+      <p>Grab Your Gift From Project!</p>
+    </Modal>
+  </teleport>
+   <teleport to=".modals" v-if="showModalTwo">
+    <Modal @click="toggleModalTwo"> 
+      <h1>Sign into the news letter!..</h1>
+      <p>For updates and promo code</p>
+    </Modal>
+   </teleport >
+
+  <button @click.alt="toggleModal">Click to Open  Modal</button>
+  <button @click.alt="toggleModalTwo">Click to Open  Modal</button>
 </template>
 
-<script> 
-import Modal from './components/Modal.vue'
+<script>
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components:{ Modal },
- data(){
-  return{
-     title: 'My First Vue App :-)',
-     header: "Sign up for Giveaway!",
-     text:'Sign in to your account!' ,
-     showModal:false
-
-  }
- },
- methods: {
-  handleClick(){
-    console.log (this.$refs.name)
-    this.$refs.name.classList.add('active')
-    this.$refs.name.focus()
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My First Vue App :-)",
+      showModal: false,
+      showModalTwo:false,
+    };
   },
-  toggleModal() {
-    this.showModal=!this.showModal
-  }
- }
-
-}
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #10c5e1;
   margin-top: 60px;
 }
-h1{
+h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
 }
-button{
+button {
   background-color: rgba(15, 138, 44, 0.193);
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,47 +1,67 @@
 <template>
-    <div class="backdrop" @click.self="closeModal">
-        <div class="modal" :class="{sale: theme==='sale'}">
-            <h1>{{ header }}</h1>
-            <p>{{ text }}</p>
-        </div>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>Default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    props:['header','text','theme'],
-    methods:{
-        closeModal(){
-            this.$emit('close')
-        }
-    }
-}
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
 </script>
 
 <style scoped>
-.modal{
-    width: 400px;
-    padding:20px;
-    margin: 100px auto;
-    background: white;
-    border-radius: 10px;
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
 }
-.backdrop{
-    top:0;
-    position: fixed;
-    background: rgba(0,0,0,0.5);
-    width:100%;
-    height: 100%;
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
 }
-.modal h1{
-    color: #03cfb4;
-    border: none;
-    padding: 0;
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
 }
-.modal.sale{
-    background: rgb(87, 220, 20);
-    color: white;
+.modal .actions {
+  text-align: center;
+  margin: 30px 0px 10px 0px;
 }
-.modal.sale h1{
-    color: white;
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: white;
+}
+.modal .sale .actions {
+  color: white;
+}
+.modal .sale .actions a {
+  color: white;
 }
 </style>
